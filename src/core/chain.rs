1be355ce259a90496e9c367ddea39982455f43b3
Xinyu Bao <baoxinyuworks@163.com>
2025-02-18 09:32:27 +0000

    revert back to tokio mutex for now. will experiment with parkinglot

--- src/core/chain.rs
+++ src/core/chain.rs
@@ -1,7 +1,8 @@
-use std::{cell::Cell, sync::{Arc, Mutex}};
+use std::{cell::Cell, sync::Arc};
 
 use anyhow::{anyhow, Error, Result};
-use futures::future::join_all;
+use futures::{future::join_all, FutureExt};
+use tokio::sync::Mutex;
 
 use crate::{
     core::{
@@ -44,14 +45,14 @@ impl Chain {
         // if so, register them in the chain.
         let mut variables: Vec<Arc<Mutex<Variable>>> = Vec::new();
         for (index, program) in programs.iter_mut().enumerate() {
-            if let Some(awaitable_variable) = program.lock().unwrap().get_awaitable_variable() {
+            if let Some(awaitable_variable) = program.blocking_lock().get_awaitable_variable() {
                 variables.push(Arc::new(Mutex::new(Variable::parse_await_variable(
                     awaitable_variable,
                     index,
                 ))));
             }
 
-            for argument in program.lock().unwrap().get_command_line().get_arguments() {
+            for argument in program.blocking_lock().get_command_line().get_arguments() {
                 let variables_in_arguments: Vec<Arc<Mutex<Variable>>> =
                     Variable::parse_variables_from_str(argument, index)?
                         .into_iter()
@@ -61,8 +62,8 @@ impl Chain {
                 if variables.len() != 0 {
                     for item in variables_in_arguments {
                         if !variables.iter().any(|v| {
-                            v.lock().unwrap().get_variable_name()
-                                == item.lock().unwrap().get_variable_name()
+                            v.blocking_lock().get_variable_name()
+                                == item.blocking_lock().get_variable_name()
                         }) {
                             variables.push(item);
                         }
@@ -88,11 +89,11 @@ impl Chain {
             let mut variables_involved: Vec<Variable> = Vec::new();
             // Get all variables involed in this program
             // Get the variables in arguments first
-            for argument in program.lock().unwrap().get_command_line().get_arguments() {
+            for argument in program.blocking_lock().get_command_line().get_arguments() {
                 variables_involved.extend(Variable::parse_variables_from_str(argument, index)?);
             }
             // Get the variables in remedy command if any
-            if let Some(remedy_command_line) = program.lock().unwrap().get_remedy_command_line() {
+            if let Some(remedy_command_line) = program.blocking_lock().get_remedy_command_line() {
                 for argument in remedy_command_line.get_arguments() {
                     variables_involved.extend(Variable::parse_variables_from_str(argument, index)?);
                 }
@@ -150,26 +151,28 @@ impl Chain {
     pub fn insert_variable(&mut self, program_index: usize) -> Result<(), Error> {
         for variable in &mut self.variables {
             // skip the `None` value variables
-            if variable.lock().unwrap().get_value().is_ok() {
+            if variable.blocking_lock().get_value().is_ok() {
                 self.programs[program_index]
                     .clone()
-                    .lock()
+                    .lock_owned()
+                    .now_or_never()
                     .unwrap()
                     .get_command_line()
                     .inject_value_to_variables(
-                        &variable.lock().unwrap().get_raw_variable_name(),
-                        variable.lock().unwrap().get_value()?,
+                        &variable.blocking_lock().get_raw_variable_name(),
+                        variable.blocking_lock().get_value()?,
                     )?;
 
                 if let Some(command_line) = self.programs[program_index]
                     .clone()
-                    .lock()
+                    .lock_owned()
+                    .now_or_never()
                     .unwrap()
                     .get_remedy_command_line()
                 {
                     command_line.inject_value_to_variables(
-                        &variable.lock().unwrap().get_raw_variable_name(),
-                        variable.lock().unwrap().get_value()?,
+                        &variable.blocking_lock().get_raw_variable_name(),
+                        variable.blocking_lock().get_value()?,
                     )?;
                 }
             }
@@ -181,14 +184,14 @@ impl Chain {
     pub fn initialize_variables_on_chain_startup(&mut self) -> Result<(), Error> {
         for variable in &mut self.variables {
             if let VariableInitializationTime::OnChainStartup(_) =
-                variable.lock().unwrap().get_initialization_time()
+                variable.blocking_lock().get_initialization_time()
             {
                 let input: String = input_message(&format!(
                     "Please input a value for {}:",
-                    variable.lock().unwrap().get_human_readable_name()
+                    variable.blocking_lock().get_human_readable_name()
                 ))?;
                 variable
-                    .lock().unwrap()
+                    .blocking_lock()
                     .register_value(input.trim().to_string());
             }
         }
@@ -216,7 +219,7 @@ impl Chain {
         program_index: usize,
     ) -> Result<(), Error> {
         for argument in self.programs[program_index]
-            .lock().unwrap()
+            .blocking_lock()
             .get_command_line()
             .get_arguments()
         {
@@ -225,7 +228,7 @@ impl Chain {
             for program_variable in &mut program_variables {
                 for variable in &mut self.variables {
                     if program_variable.get_raw_variable_name()
-                        == variable.lock().unwrap().get_raw_variable_name()
+                        == variable.blocking_lock().get_raw_variable_name()
                         && matches!(
                             program_variable.get_initialization_time(),
                             VariableInitializationTime::OnProgramExecution(_)
@@ -233,10 +236,10 @@ impl Chain {
                     {
                         let input: String = input_message(&format!(
                             "Please input a value for {}:",
-                            variable.lock().unwrap().get_human_readable_name()
+                            variable.blocking_lock().get_human_readable_name()
                         ))?;
                         variable
-                            .lock().unwrap()
+                            .blocking_lock()
                             .register_value(input.trim().to_string());
                     }
                 }
@@ -253,7 +256,7 @@ impl Chain {
         // Increment the failure count
         self.increment_failed_execution();
         // Acquire a mut reference to the program
-        let mut program = self.programs[program_index].lock().unwrap();
+        let mut program = self.programs[program_index].blocking_lock();
         // Display error message
         display_message(Level::Error, &error_message);
 
@@ -297,7 +300,7 @@ impl std::fmt::Display for Chain {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut program_names: String = String::new();
         for program in &self.programs {
-            program_names.push_str(&(program.lock().unwrap().to_string() + "\n"))
+            program_names.push_str(&(program.blocking_lock().to_string() + "\n"))
         }
         f.write_str(&format!("{}", program_names))
     }
@@ -306,8 +309,8 @@ impl std::fmt::Display for Chain {
 impl VariableGroupControl for Chain {
     fn get_value(&self, variable_name: &str) -> Result<String, Error> {
         for variable in &self.variables {
-            if variable.lock().unwrap().get_variable_name() == variable_name {
-                return Ok(variable.lock().unwrap().get_value()?);
+            if variable.blocking_lock().get_variable_name() == variable_name {
+                return Ok(variable.blocking_lock().get_value()?);
             }
         }
 
@@ -316,8 +319,8 @@ impl VariableGroupControl for Chain {
 
     fn update_value(&mut self, variable_name: &str, value: String) {
         for variable in &mut self.variables {
-            if variable.lock().unwrap().get_raw_variable_name() == variable_name {
-                variable.lock().unwrap().register_value(value);
+            if variable.blocking_lock().get_raw_variable_name() == variable_name {
+                variable.blocking_lock().register_value(value);
                 break;
             }
         }
@@ -357,7 +360,7 @@ impl Execution<ChainExecutionResult> for Chain {
                 // Get a mutable reference to the current program.
                 let program = &mut self.programs[i];
                 // Process any functions provided as arguments for the program.
-                program.lock().unwrap().execute_argument_functions().await?;
+                program.blocking_lock().execute_argument_functions().await?;
             }
 
             // Insert available variables from the chain into the program's context.
@@ -372,7 +375,7 @@ impl Execution<ChainExecutionResult> for Chain {
             // or continue with the sequential order
             if let Some(concurrency_group_number_for_this_program) = self
                 .programs[i]
-                .lock().unwrap()
+                .blocking_lock()
                 .get_concurrency_group() 
             {
                 // Determine whetehr the concurrency group can be executed
@@ -382,10 +385,14 @@ impl Execution<ChainExecutionResult> for Chain {
                         let mut tasks = Vec::new();
                         for program in &concurrency_group {
                             let program_clone = program.clone();
-                            let future = async move {
-                                program_clone.lock().unwrap().execute().await
-                            };
-                            tasks.push(future);
+                            tasks.push(
+                                tokio::spawn(
+                                    async move {
+                                        let result = program_clone.lock().await.execute().await;
+                                        result
+                                    }
+                                )
+                            );
                         }
 
                         let results = join_all(tasks).await;
@@ -417,19 +424,19 @@ impl Execution<ChainExecutionResult> for Chain {
                     Level::Logging, 
                     &format!(
                         "Concurrent program, {}, is collected...", 
-                        self.programs[i].lock().unwrap().get_command_line()
+                        self.programs[i].blocking_lock().get_command_line()
                     )
                 );
             }
 
             // Check if the program returns an awaitable variable.
             if let Some(variable) = self.programs[i]
-                .lock().unwrap()
+                .blocking_lock()
                 .get_awaitable_variable()
                 .clone()
             {
                 // Execute the program and capture its output.
-                let output: String = match self.programs[i].lock().unwrap().execute().await {
+                let output: String = match self.programs[i].blocking_lock().execute().await {
                     Ok(result) => result[0].clone().get_output(),
                     Err(error) => {
                         self.handle_program_execution_failures(i, &error.to_string())
@@ -443,7 +450,7 @@ impl Execution<ChainExecutionResult> for Chain {
                 awaitable_value = Some(output);
             } else {
                 // If there is no awaitable variable, simply execute the program.
-                match self.programs[i].lock().unwrap().execute().await {
+                match self.programs[i].blocking_lock().execute().await {
                     Ok(result) => result,
                     Err(error) => {
                         self.handle_program_execution_failures(i, &error.to_string())
